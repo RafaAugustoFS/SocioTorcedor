8b5ed437f290540b24cfc8207bd5cdee7a4f6057
Rafael Augusto <rafaelaugustofsilva@gmail.com>
2024-09-07 19:21:35 -0300

    creating a structure project

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const sequelize = require('./config/config.js')
+const router = require("./routers/router.js");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.get("/healthcheck", (req, res) => {
+  // 200 -> OK
+  return res.status(200).json({
+    msg: "Estamos vivos!",
+    alive: true,
+  });
+});
+
+sequelize.authenticate()
+.then(async () =>{
+    console.log("Conexão estabelecida com sucesso!");
+    await sequelize.sync({ }); //Sincroniza o código com a tabela
+})
+.then(() =>{
+    app.listen(8080, () =>{
+        console.log("#####");
+        console.log("Rodando na porta 8080");
+        console.log("#####");
+    })
+})
+.catch(() =>{
+    console.error( "Erro ao se conectar com o DataBase!");
+})
